7dd62f9c47866ecf5ff871c47831a2352658ce46
lucaswarburton <127803253+lucaswarburton@users.noreply.github.com>
2025-10-17 13:28:30 -0400

    fixed style errors

--- tests/test_batch_rr_calcs.py
+++ tests/test_batch_rr_calcs.py
@@ -4,8 +4,8 @@ Unit tests for the batch_rr_calcs library
 
 from src.daamsim.batch_rr_calcs import make_speed_array
 
-class TestBatchRrCalcs:
 
+class TestBatchRrCalcs:
 
     def test_make_speed_array(self):
         assert [5, 15, 25] == make_speed_array(5, 25, 10)
