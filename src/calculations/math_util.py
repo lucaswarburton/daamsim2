2a5286504421c31db90b43764a99301264154189
lucaswarburton <lucas.warburton@gmail.com>
2025-11-10 21:19:26 -0500

    fixed type hints

--- src/calculations/math_util.py
+++ src/calculations/math_util.py
@@ -2,17 +2,17 @@ from decimal import Decimal
 import math
 
 
-def cosd(x: Decimal):
-    return math.cos(math.radians(x))
+def cosd(x: Decimal) -> Decimal:
+    return Decimal(math.cos(math.radians(x)))
 
-def make_array(min: Decimal, max: Decimal, increment: Decimal) -> list[int]:
+def make_array(min: Decimal, max: Decimal, increment: Decimal) -> list[Decimal]:
     if (min > max):
             raise ValueError("Create Array: Start cannot be greater than end. Start: " + str(min) + "End: " + str(max))
     if (increment <= 0):
         raise ValueError("Create Array: Interval cannot be less than 0: " + str(increment))
     
     array = [
-        min+ i * increment
+        min + i * increment
         for i in range(int((max - min) / increment) + 1)
     ]
     return array
