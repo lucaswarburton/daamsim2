1993671b68ef4751bdb302083c9294c3e32d7ed4
Kyle Foisy <kylefoisy@cmail.carleton.ca>
2025-11-24 19:46:24 -0500

    Fixed stupid mistake

--- src/daamsim/UI/GController.py
+++ src/daamsim/UI/GController.py
@@ -16,21 +16,21 @@ class GraphController:
     
     def displayAllGraphs(self):
         data = CurrentData()
-        azimuthOncoming = data.alpha_oncoming_vect
-        azimuthOvertake = data.alpha_overtake_vect
         r_min = data.r_min_m
         r_min_overtake = data.r_min_over
         daa_spec = data.specs
         fov = daa_spec.daa_fov_deg
         daa_range = daa_spec.daa_declaration_range
+        
         rtas_speeds = daa_spec.rtas_speed_array
         intruder_speeds = daa_spec.intruder_speed_array
+        azimuth_array = daa_spec.encounter_azimuth_array
     
         for i, in_speed in enumerate(intruder_speeds):
             in_speed = round(in_speed, 3)
             for rtas_speed in rtas_speeds:
                 rtas_speed = round(rtas_speed, 3)
-                results = per_speed_plot.convert_data(azimuthOncoming[in_speed][rtas_speed], r_min[in_speed][rtas_speed], azimuthOvertake[in_speed][rtas_speed], r_min_overtake[in_speed][rtas_speed], fov, daa_range)
+                results = per_speed_plot.convert_data(azimuth_array, r_min[in_speed][rtas_speed], azimuth_array, r_min_overtake[in_speed][rtas_speed], fov, daa_range)
                 rr = results[0]
                 points = results[1]
                 plt = per_speed_plot(rtas_speed, in_speed, round(rr, 2), daa_range, fov)
