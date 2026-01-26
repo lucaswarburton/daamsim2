80eade91f6f6b8607b3b5c437e1e147c4497ed61
Kyle Foisy <kylefoisy@cmail.carleton.ca>
2026-01-26 10:37:53 -0500

    Testing Script Partial Work

---
+++ tests/comparativescript.py
@@ -0,0 +1,17 @@
+import scipy.io
+
+def testframework(matlabfilename, jsonfilename):
+    matlab_data = loadmatlabfile(matlabfilename)
+    print(matlab_data)
+
+def loadmatlabfile(filename) -> dict: 
+    dictionary = dict()
+    scipy.io.loadmat(file_name=filename, mdict=dictionary)
+    return dictionary
+
+def loadjsonfile(filename):
+    
+    
+    
+if __name__ == "__main__":
+    testframework("test.mat", "")
